752d29c801c111dda339e11c626b6e896689041f
Atuldubey98 <atuldubey017@gmail.com>
2023-04-09 00:35:42 +0530

    dev

--- client/src/instance.jsx
+++ client/src/instance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const API_URL = import.meta.env.DEV
   ? "http://localhost:9000"
-  : import.meta.env.API_URL;
+  : "https://api-connect-developer.onrender.com";
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
