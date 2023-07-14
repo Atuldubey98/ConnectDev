edcf4915e2ead7afa9d571914130509a8f0c2bb2
Atuldubey98 <atuldubey017@gmail.com>
2023-07-14 12:03:30 +0530

    fix : token

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -2,11 +2,10 @@ const { NODE_ENV } = require("../config/keys");
 
 const sendToken = async (user, statusCode, res) => {
   const token = await user.getJWTToken(user.email, user.name, user._id);
-  // options for cookie
   const options = {
     httpOnly: true,
-    sameSite: true,
-    path: "/",
+    sameSite: "strict",
+    maxAge : 3600000,
     secure: NODE_ENV !== "development",
   };
   const { email, _id, name } = user;
