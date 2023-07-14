78dec95ad5d02f004ce237b767187fce8152fc8e
Atuldubey98 <atuldubey017@gmail.com>
2023-07-14 10:57:26 +0530

    fix : .env files for prod

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,11 +1,10 @@
 const dotenv = require("dotenv");
 const path =
   process.env.NODE_ENV == "development"
-    ? ".env.development"
+    ? "../.env.development"
     : process.env.NODE_ENV == "test"
-    ? ".env.test"
-    : ".env";
-console.log(path);
+    ? "../.env.test"
+    : "../.env";
 dotenv.config({
   path,
 });
